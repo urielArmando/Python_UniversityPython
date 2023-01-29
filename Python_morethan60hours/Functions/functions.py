68a3618f4ff06df6e3c4cdb9bcba2d48ae9b4d01
Uriel Castro <uaastaiza@misena.edu.co>
2023-01-29 14:17:52 -0500

    Starting class about functions

--- Python_morethan60hours/Functions/functions.py
+++ Python_morethan60hours/Functions/functions.py
@@ -4,3 +4,54 @@ def mi_function(name, lastname):  # Function with parameter 'name'
 
 
 mi_function('Uriel', 'Castro')
+
+
+# Use return
+def sum(a, b):
+    return a + b
+
+
+result = sum(5, 3)
+print(f'Summation result: {result}')
+print(f'Summation result: {sum(5, 3)}')
+
+
+# Use default parameters in function
+# def sumar(a:int = 0, b:int = 0)  Define type of variables
+def suma(a=0, b=0) -> int:  # Define type of return, but it is not obligatory return type int
+    return a + b
+
+
+print(f'Result: {suma(2, 8)}')
+
+
+# X quantity Args in functions
+def list_names(*names):  # Names are tuple
+    for name in names:
+        print(name)
+
+
+list_names('Juan', 'Pedro', 'Uriel')
+
+
+# Exercise args in function to sum
+def summation(*numbers: int):
+    resultado = 0
+    for number in numbers:
+        resultado += number
+    return resultado
+
+
+print(summation(2, 5, 7, 10))
+
+
+def multiplication(*args: int):
+    # Use pase to only define function and not generate error
+    # pass
+    resultado = 1
+    for number in args:
+        resultado *= number
+    return resultado
+
+
+print(multiplication(2, 5, 10))
