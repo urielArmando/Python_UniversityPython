c5f4ff28e18be1266b3f030ab65dec65b18e2f4c
Uriel Castro <uaastaiza@misena.edu.co>
2023-01-29 00:51:55 -0500

    Class about function started

---
+++ Python_morethan60hours/Functions/functions.py
@@ -0,0 +1,6 @@
+# Define function
+def mi_function(name, lastname):  # Function with parameter 'name'
+    print(f'Hi your name is: {name} and your lastname is: {lastname}')
+
+
+mi_function('Uriel', 'Castro')
