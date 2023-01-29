52814c4fb653e78e1a3cc66b6fea3ec33b72569e
Uriel Castro <uaastaiza@misena.edu.co>
2023-01-28 19:20:46 -0500

    Add exercise to ranges

---
+++ Python_morethan60hours/Lists/Excercises_list.py
@@ -0,0 +1,18 @@
+# Exercise iterate range 0 to 10 only print numbers divisible by 3
+for i in range(11):
+    if i % 3 == 0:
+        print(i)
+else:
+    print('Exercise 1 ended')
+
+# Exercise create range of numbers 2 to 6 and print
+for i in range(2, 7):
+    print(i)
+else:
+    print('Exercise 2 ended')
+
+# Exercise create range 3 to 10, but increment 2 in 2
+for i in range(3, 11, 2):
+    print(i)
+else:
+    print('Exercise 3 ended')
